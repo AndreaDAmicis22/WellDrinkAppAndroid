ee221808ae92d2b536c45732fbbbe24680ebc414
SimoneAdobati <s.adobati2@campus.unimib.it>
2023-02-01 13:07:52 +0100

    Sistemato errore id

--- app/src/main/java/com/example/welldrink/MainActivity.java
+++ app/src/main/java/com/example/welldrink/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         NavHostFragment navHostFragment = (NavHostFragment)
-                getSupportFragmentManager().findFragmentById(R.id.mainFragment);
+                getSupportFragmentManager().findFragmentById(R.id.fragment_main);
         if (navHostFragment == null)
             Log.d(TAG, "Null");
         else {
             NavController navController = navHostFragment.getNavController();
             BottomNavigationView bottomNav = findViewById(R.id.home_navbar);
             AppBarConfiguration appBarConfiguration =
-                    new AppBarConfiguration.Builder(R.id.researchFragment, R.id.mainFragment, R.id.profileFragment).build();
+                    new AppBarConfiguration.Builder(R.id.fragment_main, R.id.fragment_research, R.id.fragment_profile).build();
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
             NavigationUI.setupWithNavController(bottomNav, navController);
         }
