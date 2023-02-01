2b784a770388e006b1c9adf8ec36b5e5c01bc639
Andre-869008 <a.damicis1@campus.unimib.it>
2023-02-01 17:26:50 +0100

    aggiunta cardview profile fragment con file Java

--- app/src/main/java/com/example/welldrink/ProfileFragment.java
+++ app/src/main/java/com/example/welldrink/ProfileFragment.java
@@ -2,41 +2,40 @@ package com.example.welldrink;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String TAG = ProfileFragment.class.getSimpleName();
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private List<String> drinkList;
+    private View view;
+    private ScrollView scrollView;
+    private LinearLayout linearLayout;
+    private Button FavoriteDrinkButton;
+    private Button FavoriteIngredientButton;
+    private Button TastedListButton;
+    private Button VisitedBarButton;
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public ProfileFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -58,7 +57,29 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        LinearLayout linearLayout = view.findViewById(R.id.profile_linFirst);
+        Button favoriteDrinkButton = view.findViewById(R.id.profile_grd_btnFavouriteDrink);
+        Button favoriteIngredientButton = view.findViewById(R.id.profile_grd_btnFavouriteIngredient);
+        Button tastedListButton = view.findViewById(R.id.profile_grd_btnTastedList);
+        Button visitedBarButton = view.findViewById(R.id.profile_grd_btnVisitedBar);
+        favoriteDrinkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItemCardView();
+            }
+        });
+        return view;
     }
+
+    public void addItemCardView(){
+        linearLayout.addView(copyCardView());
+    }
+
+    public CardView copyCardView(){
+        CardView cardView = view.findViewById(R.id.profile_cardview);
+        CardView newCardView = cardView;
+        return newCardView;
+    }
+
 }
