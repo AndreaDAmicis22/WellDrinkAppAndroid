57a109543ed9f9640e9a62e951df6f8891c9c114
Andre-869008 <a.damicis1@campus.unimib.it>
2023-02-03 11:55:43 +0100

    fix problem in fragment profile

--- app/src/main/java/com/example/welldrink/ui/ProfileFragment.java
+++ app/src/main/java/com/example/welldrink/ui/ProfileFragment.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,14 +25,6 @@ import java.util.List;
 public class ProfileFragment extends Fragment {
 
     private static final String TAG = ProfileFragment.class.getSimpleName();
-    private List<Drink> drinkList;
-    private View view;
-    private ScrollView scrollView;
-    private LinearLayout linearLayout;
-    private Button favoriteDrinkButton;
-    private Button favoriteIngredientButton;
-    private Button tastedListButton;
-    private Button visitedBarButton;
 
     public ProfileFragment() {
 
@@ -55,8 +45,8 @@ public class ProfileFragment extends Fragment {
         RecyclerView profileRecycleView = view.findViewById(R.id.profile_rcv);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager((requireContext()));
         profileRecycleView.setLayoutManager(linearLayoutManager);
-        Button favoriteDrinkButton = view.findViewById(R.id.profile_grd_btnFavouriteDrink);
-        Button favoriteIngredientButton = view.findViewById(R.id.profile_grd_btnFavouriteIngredient);
+        Button favoriteDrinkButton = view.findViewById(R.id.profile_grd_btnFavoriteDrink);
+        Button favoriteIngredientButton = view.findViewById(R.id.profile_grd_btnFavoriteIngredient);
         Button tastedListButton = view.findViewById(R.id.profile_grd_btnTastedList);
         Button top20Button = view.findViewById(R.id.profile_btnTop20);
 
@@ -67,7 +57,6 @@ public class ProfileFragment extends Fragment {
                 for (int i = 0; i < 1000; i++){
                     array.add(new Drink(i, Integer.toString(1),null, null, null));
                 }
-                favoriteDrinkButton.setBackgroundColor(112);
                 ProfileRecyclerViewAdapter adapter = new ProfileRecyclerViewAdapter(array);
                 profileRecycleView.setAdapter(adapter);
             }
@@ -97,17 +86,6 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        visitedBarButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                List<Drink> array = new ArrayList<>();
-                for (int i = 0; i < 1000; i++){
-                    array.add(new Drink(i, Integer.toString(4),null, null, null));
-                }
-                ProfileRecyclerViewAdapter adapter = new ProfileRecyclerViewAdapter(array);
-                profileRecycleView.setAdapter(adapter);
-            }
-        });
         top20Button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
